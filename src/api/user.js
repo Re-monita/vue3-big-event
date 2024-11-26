df7c0d7dae7dda5521909fed9e687d39c7bce28c
Re-monita <2812624384@qq.com>
2024-11-26 21:45:06 +0800

    完结撒花

--- src/api/user.js
+++ src/api/user.js
@@ -14,3 +14,7 @@ export const userUpdateInfoService = ({ id, nickname, email }) =>
 // 上传头像
 export const userUploadAvatarService = (avatar) =>
   request.patch('/my/update/avatar', { avatar })
+
+// 修改密码
+export const userUpdatePassService = ({ old_pwd, new_pwd, re_pwd }) =>
+  request.patch('/my/updatepwd', { old_pwd, new_pwd, re_pwd })
--- src/views/user/UserPassword.vue
+++ src/views/user/UserPassword.vue
@@ -1,7 +1,108 @@
-<template>
-  <div>我是密码</div>
-</template>
+<script setup>
+import { ref } from 'vue'
+import { userUpdatePassService } from '@/api/user'
+import { useUserStore } from '@/stores'
+import { useRouter } from 'vue-router'
+const pwdForm = ref({
+  old_pwd: '',
+  new_pwd: '',
+  re_pwd: ''
+})
+const pwdFormRef = ref()
 
-<script setup></script>
+const checkOldSame = (rule, value, callback) => {
+  if (value === pwdForm.value.old_pwd) {
+    callback(new Error('新密码不能与原密码相同'))
+  } else {
+    callback()
+  }
+}
+
+const checkNewSame = (rule, value, callback) => {
+  if (value !== pwdForm.value.new_pwd) {
+    callback(new Error('两次输入的密码不一致'))
+  } else {
+    callback()
+  }
+}
+
+const rules = ref({
+  old_pwd: [
+    { required: true, message: '请输入原密码', trigger: 'blur' },
+    {
+      pattern: /^\S{6,15}$/,
+      message: '密码长度必须是6-15位的非空字符串',
+      trigger: 'blur'
+    }
+  ],
+  new_pwd: [
+    { required: true, message: '请输入新密码', trigger: 'blur' },
+    {
+      pattern: /^\S{6,15}$/,
+      message: '密码长度必须是6-15位的非空字符串',
+      trigger: 'blur'
+    },
+    { validator: checkOldSame, trigger: 'blur' }
+  ],
+  re_pwd: [
+    { required: true, message: '请再次确认新密码', trigger: 'blur' },
+    {
+      pattern: /^\S{6,15}$/,
+      message: '密码长度必须是6-15位的非空字符串',
+      trigger: 'blur'
+    },
+    { validator: checkNewSame, trigger: 'blur' }
+  ]
+})
+
+const userStore = useUserStore()
+const router = useRouter()
+
+const onSubmit = async () => {
+  const valid = await pwdFormRef.value.validate()
+  if (valid) {
+    console.log(pwdForm.value)
+    await userUpdatePassService(pwdForm.value)
+    ElMessage.success('修改密码成功')
+    userStore.setToken('')
+    userStore.setUser({})
+    router.push('/login')
+  }
+}
+
+const onReset = () => {
+  pwdFormRef.value.resetFields()
+}
+</script>
+
+<template>
+  <page-container title="重置密码">
+    <el-row>
+      <el-col :span="12">
+        <el-form
+          :model="pwdForm"
+          :rules="rules"
+          ref="pwdFormRef"
+          label-width="100px"
+          size="large"
+        >
+          <el-form-item label="原密码" prop="old_pwd">
+            <el-input v-model="pwdForm.old_pwd" type="password"></el-input>
+          </el-form-item>
+          <el-form-item label="新密码" prop="new_pwd">
+            <el-input v-model="pwdForm.new_pwd" type="password"></el-input>
+          </el-form-item>
+          <el-form-item label="确认密码" prop="re_pwd">
+            <el-input v-model="pwdForm.re_pwd" type="password"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">提交</el-button>
+            <el-button @click="onReset">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+  </page-container>
+</template>
 
 <style lang="scss" scoped></style>
